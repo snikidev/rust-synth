1b281daf4516de4716bfb8b497523ea7eece30e5
snikidev <nikita.kakuev@sainsburys.co.uk>
2025-01-31 09:31:38 +0000

    Format main.py

--- api/app/main.py
+++ api/app/main.py
@@ -11,7 +11,7 @@ CLIENT = AzureOpenAI(
     azure_endpoint=os.environ["OPENAI_API_ENDPOINT"]
 )
 
-DEPLOYMENT_NAME='gpt-4o'
+DEPLOYMENT_NAME = 'gpt-4o'
 
 SYSTEM_PROMPT = {
     "role": "system",
@@ -20,28 +20,35 @@ SYSTEM_PROMPT = {
 
 app = FastAPI()
 
+
 class ValueModel(BaseModel):
     value: str
 
+
 def send_to_llm(line):
     messages = [SYSTEM_PROMPT, {"role": "user", "content": line}]
-    response = CLIENT.chat.completions.create(model=DEPLOYMENT_NAME, max_tokens=1000, messages=messages)
+    response = CLIENT.chat.completions.create(
+        model=DEPLOYMENT_NAME, max_tokens=1000, messages=messages)
     print(response)
     return [choice.message.content for choice in response.choices]
+
 
 @app.get("/healthcheck")
 def healthcheck():
     return {"status": "ok"}
 
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
 
 @app.post("/api/string")
 def read_root(value_model: ValueModel):
     print(f"Received value: {value_model.value}")
     messages = send_to_llm(value_model.value)
     return {"data": messages}
+
 
 @app.post("/api/upload")
 async def upload_csv(file: UploadFile = File(...)):
